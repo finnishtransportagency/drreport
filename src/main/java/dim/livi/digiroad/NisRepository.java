893449dacdc264ac318872535364b28287b2056e
Pasi Savolainen <pasi.savolainen@dimenteq.fi>
2017-12-27 12:58:20 +0200

    DR-866, turhat rajapinnat poistettu.

--- src/main/java/dim/livi/digiroad/NisRepository.java
+++ src/main/java/dim/livi/digiroad/NisRepository.java
@@ -3,13 +3,9 @@ package dim.livi.digiroad;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Future;
-
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -18,10 +14,8 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public class NisRepository {
-	
 	
 	protected JdbcTemplate jdbc;
 	
@@ -30,17 +24,6 @@ public class NisRepository {
         this.jdbc=jdbc;
     }
 	 
-	 
-
-	 public int getValidityManoeuvreCount(Integer[] typelist, String ToBeOrNotToBe) {
-		 String in = ToBeOrNotToBe + " (" + StringUtils.join(typelist, ',') + ")";
-	        return jdbc.queryForObject("select count(*) count from DR2USER.MANOEUVRE_ELEMENT me " +
-										"inner join DR2USER.MANOEUVRE m on me.MANOEUVRE_ID = m.ID " +
-										"where m.VALID_TO is not null and ELEMENT_TYPE " + in, itemMapper ); 
-	    }
-	 
-
-
 	public List<IdText> getAssetTypes(String term) {
 		 return jdbc.query("select ID, NAME from DR2USER.ASSET_TYPE where lower(NAME) like lower(?) order by NAME", new Object[]{term + "%"}, new RowMapperResultSetExtractor<IdText>(assetTypeMapper));
 	 }
@@ -97,16 +80,7 @@ public class NisRepository {
 		"group by ASSET_TYPE_ID, NAME, MUNICIPALITYCODE, NAME_FI, MOD_DATE " +
 		"order by to_date(MOD_DATE, 'DD-MM-YYYY')", new RowMapperResultSetExtractor<rawModifiedResult>(rawModifiedResultMapper)));
 	 }
-	
-	@Async
-	public Future<List<String>> getSleep(Integer id) throws InterruptedException {
-		Thread.sleep(3000L);
-		List<String> lista = new ArrayList<String>();
-		lista.add("ok");
-		lista.add(id.toString());
-		return new AsyncResult<List<String>>(lista);
-	}
-	
+
 	public List<String> getModDates(String startDate, String stopDate, String kunnat, String tietolajit) {
 		 return jdbc.query("select distinct to_char(cast(coalesce(ass.MODIFIED_DATE, ass.CREATED_DATE) as date), 'DD-MM-YYYY') MOD_DATE from DR2USER.ASSET ass " +
 		"inner join DR2USER.ASSET_LINK al on ass.ID = al.ASSET_ID " +
@@ -118,77 +92,7 @@ public class NisRepository {
 		"and coalesce(ass.MODIFIED_DATE, ass.CREATED_DATE) between to_date(?, 'DD-MM-YYYY') AND to_date(?, 'DD-MM-YYYY') " +
 		"order by to_date(MOD_DATE, 'DD-MM-YYYY')", new Object[]{startDate, stopDate}, new RowMapperResultSetExtractor<String>(modDateMapper));
 	 }
-	
-	public List<jqGridJsonTypeRow> getServiceUsers(Integer rows, Integer page, String sidx, String sord, String configuration) {
-		int start = (page - 1) * rows;
-		int stop = start + rows;		
-		 return jdbc.query("select USERNAME, CONFIGURATION from (" +
-							  "select a.*, ROWNUM rnum from (" +
-							    "select * from DR2USER.SERVICE_USER WHERE " + this.createWhereClause(configuration) + " ORDER BY " + sidx + " " + sord +
-							  ") a where rownum <= ?" +
-							") where rnum > ?", new Object[]{stop, start}, new RowMapperResultSetExtractor<jqGridJsonTypeRow>(serviceRowMapper));
-	 }
-	
-	 public int getServiceUserCount(String configuration) {
-	        return jdbc.queryForObject("select count(*) count from DR2USER.SERVICE_USER WHERE " + this.createWhereClause(configuration), itemMapper ); 
-	    }
 	 
-	 private String createWhereClause(String configuration) {
-		 String whereClause = "1 = 1";
-		 if ("operator".equals(configuration) || "premium".equals(configuration) || "busStopMaintainer".equals(configuration)) whereClause = "replace(CONFIGURATION, ' ', '') LIKE " + "'%\"roles\":[\"" + configuration + "\"]%'";
-		 else if ("".equals(configuration)) whereClause = "replace(CONFIGURATION, ' ', '') LIKE " + "'%\"roles\":[]%'";
-		 else if ("other".equals(configuration)) whereClause = "replace(CONFIGURATION, ' ', '') NOT LIKE '%\"roles\":[\"premium\"]%'" +
-																 " AND replace(CONFIGURATION, ' ', '') NOT LIKE '%\"roles\":[\"operator\"]%'" +
-																 " AND replace(CONFIGURATION, ' ', '') NOT LIKE '%\"roles\":[]%'";
-		 return whereClause;
-	 }
-	 
-	public List<jqGridJsonTypeRow> getValidationRules(Integer id) {
-		String whereClause = "1 = 1";
-		if (id != 0) whereClause = "ID=" + id;
-		return jdbc.query("select * from OPERAATTORI.VALIDATION_RULES WHERE " + whereClause, new Object[]{}, new RowMapperResultSetExtractor<jqGridJsonTypeRow>(validationRuleMapper));
-	 }
-	
-	@Async
-	public Future<List<ParamValue>> getValidationResult(Integer asset_type_id, String filter) {		
-		 return new AsyncResult<List<ParamValue>>(jdbc.query("with arvot as (" +
-						  "select a.ID, a.ASSET_TYPE_ID, npv.value from DR2USER.ASSET a " +
-						  "inner join DR2USER.NUMBER_PROPERTY_VALUE npv on a.ID = npv.ASSET_ID " +
-						  "where a.ID in (" +
-						    "select a.ID from DR2USER.LRM_POSITION lrm " +
-						    "inner join DR2USER.ASSET_LINK al on al.POSITION_ID = lrm.ID " +
-						    "inner join DR2USER.ASSET a on al.ASSET_ID = a.ID " +
-						    "where a.ASSET_TYPE_ID = ? AND a.VALID_TO IS null and a.FLOATING=0 and lrm.LINK_ID is not null" +
-						    ")" +
-						  ")" +
-							"select 'c' porder, 'Pienin arvo' param, min(value) value from arvot " +
-							"union " +
-							"select 'c' porder, 'Suurin arvo', max(value) from arvot " +
-							"union " +
-							"select case when count(value) " + filter + " then 'a' else 'b' end porder, param, count(value) from (" +
-							  "select case when value " + filter + " then 'Valideja' else 'Ei valideja' end param, case when value " + filter + " then 1 else 0 end value from arvot) " +
-							"group by param, value " +
-							"order by porder", new Object[]{asset_type_id}, new RowMapperResultSetExtractor<ParamValue>(ParamValueMapper)));
-	 }
-
-
-	    private static final RowMapper<Integer> itemMapper = new RowMapper<Integer>() {
-	        @Override
-	        public Integer mapRow(ResultSet rs, int rowNum) throws SQLException { 
-	            int item = rs.getInt("count");
-	            return item; 
-
-			} 
-	    };
-	    
-	    @Deprecated
-	    private static final RowMapper<Pair<Integer, String>> assetType_ = new RowMapper<Pair<Integer, String>>() {
-	        @Override
-	        public Pair<Integer, String> mapRow(ResultSet rs, int rowNum) throws SQLException {
-	        	return new ImmutablePair<Integer, String>(rs.getInt("ID"), rs.getString("NAME"));
-			} 
-	    };
-	    
 	    private static final RowMapper<IdText> assetTypeMapper = new RowMapper<IdText>() {
 	        @Override
 	        public IdText mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -200,13 +104,6 @@ public class NisRepository {
 	        @Override
 	        public IdText mapRow(ResultSet rs, int rowNum) throws SQLException {
 	        	return new IdText(rs.getInt("ID"), rs.getString("NAME_FI"));
-			} 
-	    };
-	    
-	    private static final RowMapper<IdText> AdminClassMapper = new RowMapper<IdText>() {
-	        @Override
-	        public IdText mapRow(ResultSet rs, int rowNum) throws SQLException {
-	        	return new IdText(rs.getInt("ID"), rs.getString("ADMINISTRATIVE_CLASS"));
 			} 
 	    };
 	    
@@ -224,37 +121,4 @@ public class NisRepository {
 
 			} 
 	    };
-	    
-	    @Deprecated
-	    private static final RowMapper<ServiceUser> serviceUserMapper = new RowMapper<ServiceUser>() {
-	        @Override
-	        public ServiceUser mapRow(ResultSet rs, int rowNum) throws SQLException {
-	        	return new ServiceUser(rs.getString("USERNAME"), rs.getString("CONFIGURATION"));
-			} 
-	    };
-	    
-	    private static final RowMapper<jqGridJsonTypeRow> serviceRowMapper = new RowMapper<jqGridJsonTypeRow>() {
-	        @Override
-	        public jqGridJsonTypeRow mapRow(ResultSet rs, int rowNum) throws SQLException {
-	        	return new jqGridJsonTypeRow(String.valueOf(rowNum), Arrays.asList(rs.getString("USERNAME"), rs.getString("CONFIGURATION")));
-			}
-	    };
-	        
-	    private static final RowMapper<jqGridJsonTypeRow> validationRuleMapper = new RowMapper<jqGridJsonTypeRow>() {
-	        @Override
-	        public jqGridJsonTypeRow mapRow(ResultSet rs, int rowNum) throws SQLException {
-	        	return new jqGridJsonTypeRow(String.valueOf(rs.getInt("ID")), Arrays.asList(rs.getString("TIETOLAJI"), rs.getString("TYYPPI"),
-	        			rs.getString("ARVOT"), rs.getString("MUIDEN_ARVOJEN_VAIKUTUS"), rs.getString("HUOM"), String.valueOf(rs.getInt("ASSET_TYPE_ID"))));
-			}
-	        
-	    };
-	    
-	    private static final RowMapper<ParamValue> ParamValueMapper = new RowMapper<ParamValue>() {
-	        @Override
-	        public ParamValue mapRow(ResultSet rs, int rowNum) throws SQLException {
-	        	return new ParamValue(rs.getString("PARAM"), rs.getString("VALUE"));
-			} 
-	    };
-	    
-
 }
--- src/main/java/dim/livi/digiroad/reporttool/ReportController.java
+++ src/main/java/dim/livi/digiroad/reporttool/ReportController.java
@@ -1,31 +1,22 @@
 package dim.livi.digiroad.reporttool;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import dim.livi.digiroad.NisRepository;
-import dim.livi.digiroad.Utilities;
 import dim.livi.digiroad.c3jsData;
 import dim.livi.digiroad.IdText;
-import dim.livi.digiroad.jqGridJsonType;
-import dim.livi.digiroad.jsonMessage;
+//import dim.livi.digiroad.jsonMessage;
 import dim.livi.digiroad.rawModifiedResult;
 import dim.livi.digiroad.MiddleLayer;
 
@@ -50,79 +41,26 @@ public class ReportController {
 		return new ResponseEntity<List<IdText>>(items.getAdminClass(q), HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/koodistot/kayttajat", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<jqGridJsonType> kayttajat(@RequestParam Integer page, @RequestParam Integer rows, @RequestParam String sidx, @RequestParam String sord, @RequestParam(required=false) String configuration) {
-		if ("busstop".equals(configuration)) configuration = "";
-//		else if ("all".equals(configuration)) configuration = null;
-		int userCount = items.getServiceUserCount(configuration);
-		int pageCount = (int) Math.ceil((double) userCount / rows);
-		return new ResponseEntity<jqGridJsonType>(new jqGridJsonType(pageCount, page, userCount, items.getServiceUsers(rows, page, sidx, sord, configuration)), HttpStatus.OK);
-	}
 	
 	@RequestMapping(value = "/raportit/graafi1/{startdate}/{stopdate}/{kunnat}/{tietolajit}/{hallinnollinenluokka}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<c3jsData> chart1(@PathVariable String startdate, @PathVariable String stopdate, @PathVariable String kunnat, @PathVariable String tietolajit, @PathVariable String hallinnollinenluokka) throws InterruptedException, ExecutionException {
 		MiddleLayer mid = new MiddleLayer();
-//		c3jsData chartData = mid.buildC3JsChartData(items.getModDates(startdate, stopdate, kunnat, tietolajit),
-//				mid.createArrayCombinations(kunnat, tietolajit), items.getRawModifiedResult(startdate, stopdate, kunnat, tietolajit));
-		//String today = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
 		final Future<ArrayList<rawModifiedResult>> future = items.getRawModifiedResult(startdate, stopdate, kunnat, tietolajit, hallinnollinenluokka);
 		final Future<ArrayList<rawModifiedResult>> futureAll = items.getRawModifiedAllResult(kunnat, tietolajit);
-		int startTime = ScheduleTask.getCurrentTimer();
-		jsonMessage json = new jsonMessage();
-//		SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.create();
-//		accessor.setContentType(MimeTypeUtils.TEXT_PLAIN);
-//		accessor.setNativeHeader("foo", "bar");
-//		accessor.setLeaveMutable(true);
-//		MessageHeaders headers = accessor.getMessageHeaders();
-//		Message<String> message1 = MessageBuilder.withPayload("test")
-//		        .setHeader("foo", "bar")
-//		        .build();
-//		this.template.convertAndSend("/topic/message", "message1", headers);
-		this.template.convertAndSend("/topic/message", json.createJsonMessage(Utilities.status.START.toString(), "Prosessoidaan"));
+		//int startTime = ScheduleTask.getCurrentTimer();
+		//jsonMessage json = new jsonMessage();
+		//this.template.convertAndSend("/topic/message", json.createJsonMessage(Utilities.status.START.toString(), "Prosessoidaan"));
 		while (!future.isDone() || !futureAll.isDone()) {
 			Thread.sleep(500L);
-			this.template.convertAndSend("/topic/message", json.createJsonMessage(Utilities.status.CONTINUE.toString(),"Haetaan tuloksia graafia varten, aikaa kulunut " + (ScheduleTask.getCurrentTimer() - startTime) + " s."));
+			//this.template.convertAndSend("/topic/message", json.createJsonMessage(Utilities.status.CONTINUE.toString(),"Haetaan tuloksia graafia varten, aikaa kulunut " + (ScheduleTask.getCurrentTimer() - startTime) + " s."));
 			Thread.sleep(500L);
         }
-		this.template.convertAndSend( "/topic/message", json.createJsonMessage(Utilities.status.STOP.toString(), "Haku valmis, piirretään graafi."));
+		//this.template.convertAndSend( "/topic/message", json.createJsonMessage(Utilities.status.STOP.toString(), "Haku valmis, piirretään graafi."));
 		c3jsData chartData = mid.buildC3JsChartData(items.getModDates(startdate, stopdate, kunnat, tietolajit),
 				mid.createArrayCombinations(kunnat, tietolajit), future.get(), futureAll.get());
 		chartData.setGroups(mid.createGroups(kunnat, tietolajit));
 		chartData.setCategories(mid.getGategories().toArray(new String[0]));
 		return new ResponseEntity<c3jsData>(chartData, HttpStatus.OK);
-	}
-	
-	@Autowired
-	private MailSender mailSender;
-	
-	@Autowired
-  private SimpMessagingTemplate template;
-	
-	@RequestMapping(value = "/testi", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<String> testi() throws InterruptedException, ExecutionException {
-
-        final Future<Boolean> future = mailSender.sendMail();
-        while (!future.isDone()) {
-//        	System.out.println("xx " + ScheduleTask.getCurrentTimer());
-        	Thread.sleep(1000L);
-        }
-        Boolean result = future.get();
-		return new ResponseEntity<String>(result.toString(), HttpStatus.OK);
-	}
-	
-//	@RequestMapping(value = "/testi2", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-//	public ResponseEntity<jqGridJsonType> testi2(){
-//		jqGridJsonType res = new jqGridJsonType("1", "2", "3");
-//		List<jqGridJsonTypeRow> lista = new ArrayList<jqGridJsonTypeRow>();
-//		jqGridJsonTypeRow item = new jqGridJsonTypeRow();
-//		item.setId("22");
-//		List<String> rivi = new ArrayList<String>();
-//		rivi.add("user");
-//		rivi.add("conf");
-//		item.setCell(rivi);
-//		lista.add(item);
-//		res.setRows(lista);
-//		return new ResponseEntity<jqGridJsonType>(res, HttpStatus.OK);
-//	}
+	}	
 }
 
